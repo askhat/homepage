641e299fb220bd4195dd450eddfc17d98aa918b1
Askhat Bikmetov <lolborgini@gmail.com>
2018-07-29 12:54:54 +0300

    chore: add template

---
+++ nuxt.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  head: {
+    title: 'askhat.io',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: 'Askhat&apos;s Homepage' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'stylesheet', type: 'text/css', href: 'https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css' }
+    ]
+  },
+  loading: { color: '#3B8070' },
+  build: {
+    extend (config, { isDev, isClient }) {
+      if (isDev && isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/
+        })
+      }
+    }
+  }
+}
+
